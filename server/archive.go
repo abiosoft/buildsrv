dec312c799cc08a38608119972e4f300bf9c699a
Matthew Holt <mholt@users.noreply.github.com>
2016-04-05 20:31:03 -0600

    Compression function now support directories

--- server/archive.go
+++ server/archive.go
@@ -2,56 +2,100 @@ package server
 
 import (
 	"archive/tar"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/klauspost/compress/gzip"
 	"github.com/klauspost/compress/zip"
 )
 
 // Zip creates a .zip file in the location zipPath containing
-// the contents of files listed in filePaths.
+// the contents of files listed in filePaths. Each file path
+// can be a regular file or a directory.
 func Zip(zipPath string, filePaths []string) error {
 	out, err := os.Create(zipPath)
 	if err != nil {
-		return err
+		return fmt.Errorf("error creating %s: %v", zipPath, err)
 	}
 	defer out.Close()
 
 	w := zip.NewWriter(out)
 	for _, fpath := range filePaths {
-		infile, err := os.Open(fpath)
-		if err != nil {
-			return err
-		}
-
-		outfile, err := w.Create(filepath.Base(fpath))
+		err = zipFile(w, fpath)
 		if err != nil {
 			w.Close()
-			infile.Close()
 			return err
 		}
-
-		_, err = io.Copy(outfile, infile)
-		if err != nil {
-			w.Close()
-			infile.Close()
-			return err
-		}
-
-		infile.Close()
 	}
 
 	return w.Close()
 }
 
+func zipFile(w *zip.Writer, source string) error {
+	sourceInfo, err := os.Stat(source)
+	if err != nil {
+		return fmt.Errorf("error stat'ing %s: %v", source, err)
+	}
+
+	var baseDir string
+	if sourceInfo.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	return filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("error walking to %s: %v", path, err)
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return fmt.Errorf("error making header for %s: %v", path, err)
+		}
+
+		if baseDir != "" {
+			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+		}
+
+		if info.IsDir() {
+			header.Name += string(filepath.Separator)
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		writer, err := w.CreateHeader(header)
+		if err != nil {
+			return fmt.Errorf("error making header for %s: %v", path, err)
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return fmt.Errorf("error opening %s: %v", path, err)
+		}
+		defer file.Close()
+
+		_, err = io.Copy(writer, file)
+		if err != nil {
+			return fmt.Errorf("error copying contents of %s: %v", path, err)
+		}
+
+		return nil
+	})
+}
+
 // TarGz creates a .tar.gz file at targzPath containing
-// the contents of files listed in filePaths.
+// the contents of files listed in filePaths. Any file
+// path can be a regular file or a directory.
 func TarGz(targzPath string, filePaths []string) error {
 	out, err := os.Create(targzPath)
 	if err != nil {
-		return err
+		return fmt.Errorf("error creating %s: %v", targzPath, err)
 	}
 	defer out.Close()
 
@@ -62,37 +106,64 @@ func TarGz(targzPath string, filePaths []string) error {
 	defer tarWriter.Close()
 
 	for _, fpath := range filePaths {
-		infile, err := os.Open(fpath)
+		err := tarGzFile(tarWriter, fpath)
 		if err != nil {
 			return err
 		}
-
-		infileInfo, err := infile.Stat()
-		if err != nil {
-			infile.Close()
-			return err
-		}
-
-		fileHeader, err := tar.FileInfoHeader(infileInfo, fpath)
-		if err != nil {
-			infile.Close()
-			return err
-		}
-
-		err = tarWriter.WriteHeader(fileHeader)
-		if err != nil {
-			infile.Close()
-			return err
-		}
-
-		_, err = io.Copy(tarWriter, infile)
-		if err != nil {
-			infile.Close()
-			return err
-		}
-
-		infile.Close()
 	}
 
 	return nil
+}
+
+func tarGzFile(tarWriter *tar.Writer, source string) error {
+	sourceInfo, err := os.Stat(source)
+	if err != nil {
+		return fmt.Errorf("error stat'ing %s: %v", source, err)
+	}
+
+	var baseDir string
+	if sourceInfo.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	return filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("error walking to %s: %v", path, err)
+		}
+
+		header, err := tar.FileInfoHeader(info, path)
+		if err != nil {
+			return fmt.Errorf("error making header for %s: %v", path, err)
+		}
+
+		if baseDir != "" {
+			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+		}
+
+		if info.IsDir() {
+			header.Name += "/"
+		}
+
+		err = tarWriter.WriteHeader(header)
+		if err != nil {
+			return fmt.Errorf("error writing header for %s: %v", path, err)
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return fmt.Errorf("error opening %s: %v", path, err)
+		}
+		defer file.Close()
+
+		_, err = io.Copy(tarWriter, file)
+		if err != nil {
+			return fmt.Errorf("error copying contents of %s: %v", path, err)
+		}
+
+		return nil
+	})
 }
